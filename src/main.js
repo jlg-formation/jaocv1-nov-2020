4188eb1da754fdf12a26141cc120a784da75be67
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-11-03 11:46:18 +0100

    added label

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 const SVGNS = "http://www.w3.org/2000/svg";
 
+function getAngle(n, c) {
+  return (n * (Math.PI * 2)) / c.nbr;
+}
+
+function getPoint(n, c) {
+  const angle = getAngle(n, c);
+  return { x: c.cx + c.r * Math.cos(angle), y: c.cy + c.r * Math.sin(angle) };
+}
+
 function addSmallCircles(c) {
   const svg = document.querySelector("svg");
   for (let i = 0; i < c.nbr; i++) {
-    const angle = (i * (Math.PI * 2)) / c.nbr;
-    const x = c.cx + c.r * Math.cos(angle);
-    const y = c.cy + c.r * Math.sin(angle);
+    const { x, y } = getPoint(i, c);
+
     const circle = document.createElementNS(SVGNS, "circle");
     circle.setAttributeNS(null, "cx", x);
     circle.setAttributeNS(null, "cy", y);
@@ -14,23 +22,33 @@ function addSmallCircles(c) {
     circle.setAttributeNS(null, "stroke", "black");
     circle.setAttributeNS(null, "stroke-width", "3");
     svg.appendChild(circle);
+
+    const p = getPoint(i, {
+      r: c.r + 30,
+      cx: c.cx,
+      cy: c.cy,
+      nbr: c.nbr,
+    });
+
+    const text = document.createElementNS(SVGNS, "text");
+    text.setAttributeNS(null, "x", p.x);
+    text.setAttributeNS(null, "y", p.y);
+    text.setAttributeNS(null, "fill", "black");
+    text.setAttributeNS(null, "text-anchor", "middle");
+    text.appendChild(document.createTextNode(i));
+    svg.appendChild(text);
   }
 }
 
 function addLine(n1, n2, c) {
   const svg = document.querySelector("svg");
-  const angle1 = (n1 * (Math.PI * 2)) / c.nbr;
-  const angle2 = (n2 * (Math.PI * 2)) / c.nbr;
-
-  const x1 = c.cx + c.r * Math.cos(angle1);
-  const y1 = c.cy + c.r * Math.sin(angle1);
-  const x2 = c.cx + c.r * Math.cos(angle2);
-  const y2 = c.cy + c.r * Math.sin(angle2);
+  const p1 = getPoint(n1, c);
+  const p2 = getPoint(n2, c);
   const line = document.createElementNS(SVGNS, "line");
-  line.setAttributeNS(null, "x1", x1);
-  line.setAttributeNS(null, "y1", y1);
-  line.setAttributeNS(null, "x2", x2);
-  line.setAttributeNS(null, "y2", y2);
+  line.setAttributeNS(null, "x1", p1.x);
+  line.setAttributeNS(null, "y1", p1.y);
+  line.setAttributeNS(null, "x2", p2.x);
+  line.setAttributeNS(null, "y2", p2.y);
   line.setAttributeNS(null, "stroke", "blue");
   line.setAttributeNS(null, "stroke-width", "3");
   svg.appendChild(line);
