4ed7fc46c9e7bbbf0738f70ee5fb235e4a1ba887
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-11-04 15:12:53 +0100

    added Config

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
+import { Config } from "./Config";
 import { init, sync } from "./multiplication";
 
 function main() {
-  const c = {
+  const c: Config = {
     r: 200,
     cx: 400,
     cy: 250,
     nbr: 500,
-    multi: 40,
+    multi: 2,
     showText: true,
   };
   init(c);
