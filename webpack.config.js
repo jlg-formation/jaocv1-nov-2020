a516921b83fdd80757bef997405be1ef9cfd8aee
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-11-04 12:05:45 +0100

    added clean webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path = require("path");
 
 module.exports = {
@@ -9,6 +10,7 @@ module.exports = {
     filename: "bundle.js",
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "src/index.html",
     }),
